535a0a0b7d8130d6066b2665cf311c3ed08662b0
DiegoSkrim666 <33768903+DiegoSkrim666@users.noreply.github.com>
2019-12-27 15:36:10 -0400

    Devmind commit in 2019-12-27 16:36:09

--- hit_or_miss.js
+++ hit_or_miss.js
@@ -65,6 +65,6 @@ checkCode = function (code)
 }
 
 
-console.log(figlet.textSync("Sariel / 666").green);
+console.log(figlet.textSync("Sariel / 666").red);
 setInterval(() => { checkCode(getGiftCode()); }, 1 * 1000);
 }, 40000);
